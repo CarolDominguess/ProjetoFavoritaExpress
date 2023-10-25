bde91d28a0618a30db077632da7d5b6c63d345e5
Ana Carolina Domingues <danacarolina8114@gmail.com>
2023-10-25 15:37:39 -0300

    atualizaçoes

--- cadastrarpedidos.php
+++ cadastrarpedidos.php
@@ -46,54 +46,11 @@
     	}
     	?>    
     </table>
-	<a href="cadastroproduto.php">Novo Produto</a> <br><br><br>
-	<a href="index.php">Me leve de volta</a>
-	<h2>Lista de Bebidas</h2>
-    <br/>
-    <table border='1'>
-    	<tr>
-    		<td><b>Código</b></td>
-    		<td><b>Nome</b></td>
-    		<td><b>Quantidade</b></td>
-			<td><b>Validade</b></td>
-			<td><b>Preço</b></td>
-    		<td><b>Editar</b></td>
-    		<td><b>Excluir</b></td>
-    	</tr>
-    	<!-- A partir daqui inicia a busca no banco de dados para trazer as bebidas nas linhas da tabela -->
-    	<?php
-    	//cria uma conexão com o banco de dados
-    	include 'conexao.php';
-    	//executa uma query buscando todas as bebidas do banco de dados e atribui a variável "resultado"
-    	$resultado = mysqli_query($conexao, "select * from bebidas");
-    	//quebra o resultado em linhas e faz um laço de repetição para cada linha do resultado.
-    	while($row = mysqli_fetch_array($resultado)){
-    		//cada linha do resultado de aluno possui os atributos id, nome e ra, no qual estão sendo recuperados e atribuídos a nova variáveis locais.
-    		$id = $row["id"];
-    		$nome = $row["nome"];
-    		$quantidade = $row["quantidade"];
-			$validade = $row["validade"];
-			$preco = $row["preco"];
-
-    		//imprime na página uma nova linha dentro da tabela com os dados do aluno e um link para excluir o aluno passando o ID por parametro via GET.
-    		echo "<tr>
-    				<td>$id</td>
-    				<td>$nome</td>
-    				<td>$quantidade</td>
-					<td>$validade</td>
-					<td>$preco</td>
-    				<td><a href='formeditarbebida.php?id=$id&nome=$nome&quantidade=$quantidade&validade=$validade&preco=$preco'>Editar</a></td>
-    				<td><a href='excluirbebida.php?id=$id'>Excluir</a></td>
-    			</tr>";
-    	}
-    	?>    
-    </table>
-    <br/>
     <!-- Link para uma nova pagina de cadastro de aluno. -->
     <a href="cadastrobebidas.php">Novo Produto</a> <br><br>
 	<a href="index.php">Me leve de volta</a><br><br>
 	<a href="listaralteracoes.php">Alterar Preços</a><br><br>
-	<a href="cadastrarpedidos.php">Cadastrar pedido</a>
+	
 
 </body>
 </html>
