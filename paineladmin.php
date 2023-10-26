76da9319ecccf89f2a92dac86da5b2059a7fd184
Ana Carolina Domingues <danacarolina8114@gmail.com>
2023-10-26 17:28:32 -0300

    atualizaçoes

---
+++ paineladmin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles/painel.css">
+</head>
+<body>
+    <div id="links">
+    <a href="listarproduto.php">ESTOQUE DE PRODUTOS</a> <br><br>
+	<a href="listarpedidos.php">PAINEL DE PEDIDOS</a><br><br>
+	<a href="listaralteracoes.php">ALTERAR PREÇOS</a><br><br>
+    <a href="telacozinhas.php">PAINEL DA COZINHA</a><br><br>
+	<a href="index.php">SAIR</a>
+    </div>
+</body>
+</html>
