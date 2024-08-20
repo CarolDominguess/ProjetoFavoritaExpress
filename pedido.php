651a1bdda61e79e4ae5c6b2fd3562b8442c511f9
Ana Carolina Domingues <danacarolina8114@gmail.com>
2024-08-20 20:31:16 -0300

    updates

---
+++ pedido.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@700&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/styles.css">
+    
+    
+    <title>Favorita Express</title>
+</head> 
+
+<body>
+    <header>
+        <div class="cabecalho">
+		    <div id="logo">
+			<img id="logo1" src="./imagens/logo.png" width="290" height="255">
+		    </div>
+        </div>
+
+    <div id="menu">
+    <ul>
+        <li><a class="exemplo" id="home" href="index.php"></a></li>
+        <li><a class="exemplo" href="sobre.php"></a></li>
+        <li><a class="exemplo" href="tamanho.php"></a></li>
+        <li><a class="exemplo" href="promos.php" ></a></li>
+        <li><a class="exemplo" href="acesse.php"></a></li>
+        
+    </div>
+    </ul>
+
+    
+    </header>
+    <h2 id="texto">Faça seu pedido agora!</h2>
+    <div id="botaoembaixo">
+        <a href="#" id="botaobaixo"><li id="textbaixo">FINALIZAR</li></a>
+    </div>
+</body>
+</html>
